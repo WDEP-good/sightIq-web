55b64c176f5725493089a5a450699bdd04d9e2b1
WDEP <2633880953@qq.com>
2025-05-20 16:26:24 +0800

    first commit

---
+++ src/layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="layout" />
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+import DefaultLayout from "./default.vue";
+
+const route = useRoute();
+const layout = computed(() => {
+  return DefaultLayout;
+});
+</script>
+
+<style scoped></style>
